3956de4a36d3547d3c301353ea12da1a49f034d0
jjsecko <jineckos@gmail.com>
2016-06-18 10:30:17 -0400

    examen

---
+++ routes/bjetoEjemplo.js
@@ -0,0 +1,5 @@
+var libro={titulo:"condor", autor:"alvares", paginas:"250", editorial:"don bosco", costo:"75"}
+for (i in libro ){
+            console.log(i+": " +libro[i]);
+
+}
---
+++ routes/tablaResta.js
@@ -0,0 +1,9 @@
+var i ;
+var n=10;
+var dig=5;
+for (i=1;i<=n;i++)
+{
+console.log(dig + " - " + i + " = " + (dig-i));
+} 
+
+
